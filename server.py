a4f7324da50e958883779b6d913c9543fa899a41
Nathan R Smith <nrsmith012@gmail.com>
2017-08-06 21:07:28 -0700

    basicaly okay

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
-from flask import Flask, redirect, render_template, session, url_for
+from flask import Flask, redirect, render_template, request, session, url_for
 app = Flask(__name__)
-app.secret_key = 'A1gr81337C()D35z4Uok1o1'
+app.secret_key = 'A++gr8,1337C()D354Uok1o1'
 
 
 @app.route('/')
@@ -8,12 +8,22 @@ def index():
     if 'xserved' not in session:
         session['xserved'] = 0
     session['xserved'] += 1
-    print(session['xserved'])
     return render_template('index.html', 
                             xserved = session['xserved'],
                             css_path = url_for('static', filename='style.css'),
                             )
 
+@app.route('/plus_2')
+def add2():
+    if 'xserved' not in session:
+        session['xserved'] = 0
+    session['xserved'] += 2
+    return render_template('index.html', 
+                            xserved = session['xserved'],
+                            css_path = url_for('static', filename='style.css'),
+                            )
+
+@app.route('/reset')
 def reset():
     session['xserved'] = 0
     return redirect(url_for('index'))
